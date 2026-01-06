97eb34b80230b88d66335e66a4da9676efa3ec3c
quantum-corn <akashkhetan044@gmail.com>
2026-01-06 17:30:54 +0530

    DEV v0.2: Seeding route and placeholder data

---
+++ lib/placeholder-data.ts
@@ -0,0 +1,50 @@
+const users = [
+  {
+    id: "410544b2-4001-4271-9855-fec4b6a6442a",
+    name: "Akash",
+    email: "akashkhetan044@gmail.com",
+    password: "autobotsrollout",
+  },
+  {
+    id: "13D07535-C59E-4157-A011-F8D2EF4E0CBB",
+    name: "Rumana",
+    email: "rumana.i.fazir@gmail.com",
+    password: "rumana@diddit",
+  },
+];
+
+const tasks = [
+  {
+    id: "d6e15727-9fe1-4961-8c5b-ea44a9bd81aa",
+    user_id: "410544b2-4001-4271-9855-fec4b6a6442a",
+    title: "Seed",
+    description: "Create seed data; Create seed route;",
+    status: "pending",
+    createdDate: "2026-Jan-06 165000+0530",
+    updatedDate: "2026-Jan-06 165000+0530",
+    dueDate: "2026-Jan-06 220000+0530",
+  },
+  {
+    id: "3958dc9e-712f-4377-85e9-fec4b6a6442a",
+    user_id: "13D07535-C59E-4157-A011-F8D2EF4E0CBB",
+    title: "Design the frontend",
+    description: "Discuss the features; Create a design; Provide assets;",
+    status: "pending",
+    createdDate: "2026-Jan-06 165100+0530",
+    updatedDate: "2026-Jan-06 165100+0530",
+    dueDate: "2026-Jan-08 220000+0530",
+  },
+  {
+    id: "3958dc9e-742f-4377-85e9-fec4b6a6442a",
+    user_id: "410544b2-4001-4271-9855-fec4b6a6442a",
+    title: "Implement authentication",
+    description:
+      "Implement the authentication backend; Implement the authentication frontend;",
+    status: "pending",
+    createdDate: "2026-Jan-06 165500+0530",
+    updatedDate: "2026-Jan-06 165500+0530",
+    dueDate: "2026-Jan-13 220000+0530",
+  },
+];
+
+export { users, tasks };
