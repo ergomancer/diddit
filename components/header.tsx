02215de33f626a9ab5e73cad1ac6ab80632aa5ef
quantum-corn <akashkhetan044@gmail.com>
2026-01-21 10:47:08 +0530

    FIX v6.1: Fix the bug where mode-toggle takes home

--- components/header.tsx
+++ components/header.tsx
@@ -7,8 +7,8 @@ export default function Header() {
     <header className="px-2 md:px-3 lg:px-4 xl:px-4.5 py-2 md:py-1.5 lg:py-1 xl:py-0.5 border-b backdrop-blur-xs">
       <Link href={"/"}>
         <Diddit />
-        <ModeToggle />
       </Link>
+        <ModeToggle />
     </header>
   );
 }
